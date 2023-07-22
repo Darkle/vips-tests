f5ee8667a5a7775ce6dc623d09eb547eade60a58
Coop <coop.coding@gmail.com>
2023-07-22 18:21:33 +1000

    Updates

--- Program.fs
+++ Program.fs
@@ -47,165 +47,165 @@ let convertImageToArchiveFormat fileName =
 let main _ =
     let sw = System.Diagnostics.Stopwatch.StartNew()
 
-    createThumbnail ("large-image1.jpg")
-    createThumbnail ("large-image2.jpg")
-    createThumbnail ("large-image3.jpg")
-    createThumbnail ("large-image4.jpg")
-    createThumbnail ("large-image5.jpg")
-    createThumbnail ("large-image6.jpg")
-    createThumbnail ("large-image7.jpg")
-    createThumbnail ("large-image8.jpg")
-    createThumbnail ("large-image9.jpg")
-    createThumbnail ("large-image10.jpg")
-    createThumbnail ("large-image11.jpg")
-    createThumbnail ("large-image12.jpg")
-    createThumbnail ("large-image13.jpg")
-    createThumbnail ("large-image14.jpg")
-    createThumbnail ("large-image15.jpg")
-    createThumbnail ("large-image16.jpg")
-    createThumbnail ("large-image17.jpg")
-    createThumbnail ("large-image18.jpg")
-    createThumbnail ("large-image19.jpg")
-    createThumbnail ("large-image20.jpg")
-    createThumbnail ("large-image21.jpg")
-    createThumbnail ("large-image22.jpg")
-    createThumbnail ("large-image23.jpg")
-    createThumbnail ("large-image24.jpg")
-    createThumbnail ("large-image25.jpg")
-    createThumbnail ("large-image26.jpg")
-    createThumbnail ("large-image27.jpg")
-    createThumbnail ("large-image28.jpg")
-    createThumbnail ("large-image29.jpg")
-    createThumbnail ("large-image30.jpg")
-    createThumbnail ("large-image31.jpg")
-    createThumbnail ("large-image32.jpg")
-    createThumbnail ("large-image33.jpg")
-    createThumbnail ("large-image34.jpg")
-    createThumbnail ("large-image35.jpg")
-    createThumbnail ("large-image36.jpg")
-    createThumbnail ("large-image37.jpg")
-    createThumbnail ("large-image38.jpg")
-    createThumbnail ("large-image39.jpg")
-    createThumbnail ("large-image40.jpg")
-    createThumbnail ("large-image41.jpg")
-    createThumbnail ("large-image42.jpg")
-    createThumbnail ("large-image43.jpg")
-    createThumbnail ("large-image44.jpg")
-    createThumbnail ("large-image45.jpg")
-    createThumbnail ("large-image46.jpg")
-    createThumbnail ("large-image47.jpg")
-    createThumbnail ("large-image48.jpg")
-    createThumbnail ("large-image49.jpg")
-    createThumbnail ("large-image50.jpg")
-    createThumbnail ("large-image51.jpg")
+    createThumbnail "large-image1.jpg" |> ignore
+    createThumbnail "large-image2.jpg" |> ignore
+    createThumbnail "large-image3.jpg" |> ignore
+    createThumbnail "large-image4.jpg" |> ignore
+    createThumbnail "large-image5.jpg" |> ignore
+    createThumbnail "large-image6.jpg" |> ignore
+    createThumbnail "large-image7.jpg" |> ignore
+    createThumbnail "large-image8.jpg" |> ignore
+    createThumbnail "large-image9.jpg" |> ignore
+    createThumbnail "large-image10.jpg" |> ignore
+    createThumbnail "large-image11.jpg" |> ignore
+    createThumbnail "large-image12.jpg" |> ignore
+    createThumbnail "large-image13.jpg" |> ignore
+    createThumbnail "large-image14.jpg" |> ignore
+    createThumbnail "large-image15.jpg" |> ignore
+    createThumbnail "large-image16.jpg" |> ignore
+    createThumbnail "large-image17.jpg" |> ignore
+    createThumbnail "large-image18.jpg" |> ignore
+    createThumbnail "large-image19.jpg" |> ignore
+    createThumbnail "large-image20.jpg" |> ignore
+    createThumbnail "large-image21.jpg" |> ignore
+    createThumbnail "large-image22.jpg" |> ignore
+    createThumbnail "large-image23.jpg" |> ignore
+    createThumbnail "large-image24.jpg" |> ignore
+    createThumbnail "large-image25.jpg" |> ignore
+    createThumbnail "large-image26.jpg" |> ignore
+    createThumbnail "large-image27.jpg" |> ignore
+    createThumbnail "large-image28.jpg" |> ignore
+    createThumbnail "large-image29.jpg" |> ignore
+    createThumbnail "large-image30.jpg" |> ignore
+    createThumbnail "large-image31.jpg" |> ignore
+    createThumbnail "large-image32.jpg" |> ignore
+    createThumbnail "large-image33.jpg" |> ignore
+    createThumbnail "large-image34.jpg" |> ignore
+    createThumbnail "large-image35.jpg" |> ignore
+    createThumbnail "large-image36.jpg" |> ignore
+    createThumbnail "large-image37.jpg" |> ignore
+    createThumbnail "large-image38.jpg" |> ignore
+    createThumbnail "large-image39.jpg" |> ignore
+    createThumbnail "large-image40.jpg" |> ignore
+    createThumbnail "large-image41.jpg" |> ignore
+    createThumbnail "large-image42.jpg" |> ignore
+    createThumbnail "large-image43.jpg" |> ignore
+    createThumbnail "large-image44.jpg" |> ignore
+    createThumbnail "large-image45.jpg" |> ignore
+    createThumbnail "large-image46.jpg" |> ignore
+    createThumbnail "large-image47.jpg" |> ignore
+    createThumbnail "large-image48.jpg" |> ignore
+    createThumbnail "large-image49.jpg" |> ignore
+    createThumbnail "large-image50.jpg" |> ignore
+    createThumbnail "large-image51.jpg" |> ignore
     createThumbnail "dog.jpg" |> ignore
     createThumbnail "person.jpg" |> ignore
 
-    convertImageToOptimizedFormat ("large-image1.jpg")
-    convertImageToOptimizedFormat ("large-image2.jpg")
-    convertImageToOptimizedFormat ("large-image3.jpg")
-    convertImageToOptimizedFormat ("large-image4.jpg")
-    convertImageToOptimizedFormat ("large-image5.jpg")
-    convertImageToOptimizedFormat ("large-image6.jpg")
-    convertImageToOptimizedFormat ("large-image7.jpg")
-    convertImageToOptimizedFormat ("large-image8.jpg")
-    convertImageToOptimizedFormat ("large-image9.jpg")
-    convertImageToOptimizedFormat ("large-image10.jpg")
-    convertImageToOptimizedFormat ("large-image11.jpg")
-    convertImageToOptimizedFormat ("large-image12.jpg")
-    convertImageToOptimizedFormat ("large-image13.jpg")
-    convertImageToOptimizedFormat ("large-image14.jpg")
-    convertImageToOptimizedFormat ("large-image15.jpg")
-    convertImageToOptimizedFormat ("large-image16.jpg")
-    convertImageToOptimizedFormat ("large-image17.jpg")
-    convertImageToOptimizedFormat ("large-image18.jpg")
-    convertImageToOptimizedFormat ("large-image19.jpg")
-    convertImageToOptimizedFormat ("large-image20.jpg")
-    convertImageToOptimizedFormat ("large-image21.jpg")
-    convertImageToOptimizedFormat ("large-image22.jpg")
-    convertImageToOptimizedFormat ("large-image23.jpg")
-    convertImageToOptimizedFormat ("large-image24.jpg")
-    convertImageToOptimizedFormat ("large-image25.jpg")
-    convertImageToOptimizedFormat ("large-image26.jpg")
-    convertImageToOptimizedFormat ("large-image27.jpg")
-    convertImageToOptimizedFormat ("large-image28.jpg")
-    convertImageToOptimizedFormat ("large-image29.jpg")
-    convertImageToOptimizedFormat ("large-image30.jpg")
-    convertImageToOptimizedFormat ("large-image31.jpg")
-    convertImageToOptimizedFormat ("large-image32.jpg")
-    convertImageToOptimizedFormat ("large-image33.jpg")
-    convertImageToOptimizedFormat ("large-image34.jpg")
-    convertImageToOptimizedFormat ("large-image35.jpg")
-    convertImageToOptimizedFormat ("large-image36.jpg")
-    convertImageToOptimizedFormat ("large-image37.jpg")
-    convertImageToOptimizedFormat ("large-image38.jpg")
-    convertImageToOptimizedFormat ("large-image39.jpg")
-    convertImageToOptimizedFormat ("large-image40.jpg")
-    convertImageToOptimizedFormat ("large-image41.jpg")
-    convertImageToOptimizedFormat ("large-image42.jpg")
-    convertImageToOptimizedFormat ("large-image43.jpg")
-    convertImageToOptimizedFormat ("large-image44.jpg")
-    convertImageToOptimizedFormat ("large-image45.jpg")
-    convertImageToOptimizedFormat ("large-image46.jpg")
-    convertImageToOptimizedFormat ("large-image47.jpg")
-    convertImageToOptimizedFormat ("large-image48.jpg")
-    convertImageToOptimizedFormat ("large-image49.jpg")
-    convertImageToOptimizedFormat ("large-image50.jpg")
-    convertImageToOptimizedFormat ("large-image51.jpg")
+    convertImageToOptimizedFormat "large-image1.jpg" |> ignore
+    convertImageToOptimizedFormat "large-image2.jpg" |> ignore
+    convertImageToOptimizedFormat "large-image3.jpg" |> ignore
+    convertImageToOptimizedFormat "large-image4.jpg" |> ignore
+    convertImageToOptimizedFormat "large-image5.jpg" |> ignore
+    convertImageToOptimizedFormat "large-image6.jpg" |> ignore
+    convertImageToOptimizedFormat "large-image7.jpg" |> ignore
+    convertImageToOptimizedFormat "large-image8.jpg" |> ignore
+    convertImageToOptimizedFormat "large-image9.jpg" |> ignore
+    convertImageToOptimizedFormat "large-image10.jpg" |> ignore
+    convertImageToOptimizedFormat "large-image11.jpg" |> ignore
+    convertImageToOptimizedFormat "large-image12.jpg" |> ignore
+    convertImageToOptimizedFormat "large-image13.jpg" |> ignore
+    convertImageToOptimizedFormat "large-image14.jpg" |> ignore
+    convertImageToOptimizedFormat "large-image15.jpg" |> ignore
+    convertImageToOptimizedFormat "large-image16.jpg" |> ignore
+    convertImageToOptimizedFormat "large-image17.jpg" |> ignore
+    convertImageToOptimizedFormat "large-image18.jpg" |> ignore
+    convertImageToOptimizedFormat "large-image19.jpg" |> ignore
+    convertImageToOptimizedFormat "large-image20.jpg" |> ignore
+    convertImageToOptimizedFormat "large-image21.jpg" |> ignore
+    convertImageToOptimizedFormat "large-image22.jpg" |> ignore
+    convertImageToOptimizedFormat "large-image23.jpg" |> ignore
+    convertImageToOptimizedFormat "large-image24.jpg" |> ignore
+    convertImageToOptimizedFormat "large-image25.jpg" |> ignore
+    convertImageToOptimizedFormat "large-image26.jpg" |> ignore
+    convertImageToOptimizedFormat "large-image27.jpg" |> ignore
+    convertImageToOptimizedFormat "large-image28.jpg" |> ignore
+    convertImageToOptimizedFormat "large-image29.jpg" |> ignore
+    convertImageToOptimizedFormat "large-image30.jpg" |> ignore
+    convertImageToOptimizedFormat "large-image31.jpg" |> ignore
+    convertImageToOptimizedFormat "large-image32.jpg" |> ignore
+    convertImageToOptimizedFormat "large-image33.jpg" |> ignore
+    convertImageToOptimizedFormat "large-image34.jpg" |> ignore
+    convertImageToOptimizedFormat "large-image35.jpg" |> ignore
+    convertImageToOptimizedFormat "large-image36.jpg" |> ignore
+    convertImageToOptimizedFormat "large-image37.jpg" |> ignore
+    convertImageToOptimizedFormat "large-image38.jpg" |> ignore
+    convertImageToOptimizedFormat "large-image39.jpg" |> ignore
+    convertImageToOptimizedFormat "large-image40.jpg" |> ignore
+    convertImageToOptimizedFormat "large-image41.jpg" |> ignore
+    convertImageToOptimizedFormat "large-image42.jpg" |> ignore
+    convertImageToOptimizedFormat "large-image43.jpg" |> ignore
+    convertImageToOptimizedFormat "large-image44.jpg" |> ignore
+    convertImageToOptimizedFormat "large-image45.jpg" |> ignore
+    convertImageToOptimizedFormat "large-image46.jpg" |> ignore
+    convertImageToOptimizedFormat "large-image47.jpg" |> ignore
+    convertImageToOptimizedFormat "large-image48.jpg" |> ignore
+    convertImageToOptimizedFormat "large-image49.jpg" |> ignore
+    convertImageToOptimizedFormat "large-image50.jpg" |> ignore
+    convertImageToOptimizedFormat "large-image51.jpg" |> ignore
     convertImageToOptimizedFormat "dog.jpg" |> ignore
     convertImageToOptimizedFormat "person.jpg" |> ignore
 
-    convertImageToArchiveFormat ("large-image1.jpg")
-    convertImageToArchiveFormat ("large-image2.jpg")
-    convertImageToArchiveFormat ("large-image3.jpg")
-    convertImageToArchiveFormat ("large-image4.jpg")
-    convertImageToArchiveFormat ("large-image5.jpg")
-    convertImageToArchiveFormat ("large-image6.jpg")
-    convertImageToArchiveFormat ("large-image7.jpg")
-    convertImageToArchiveFormat ("large-image8.jpg")
-    convertImageToArchiveFormat ("large-image9.jpg")
-    convertImageToArchiveFormat ("large-image10.jpg")
-    convertImageToArchiveFormat ("large-image11.jpg")
-    convertImageToArchiveFormat ("large-image12.jpg")
-    convertImageToArchiveFormat ("large-image13.jpg")
-    convertImageToArchiveFormat ("large-image14.jpg")
-    convertImageToArchiveFormat ("large-image15.jpg")
-    convertImageToArchiveFormat ("large-image16.jpg")
-    convertImageToArchiveFormat ("large-image17.jpg")
-    convertImageToArchiveFormat ("large-image18.jpg")
-    convertImageToArchiveFormat ("large-image19.jpg")
-    convertImageToArchiveFormat ("large-image20.jpg")
-    convertImageToArchiveFormat ("large-image21.jpg")
-    convertImageToArchiveFormat ("large-image22.jpg")
-    convertImageToArchiveFormat ("large-image23.jpg")
-    convertImageToArchiveFormat ("large-image24.jpg")
-    convertImageToArchiveFormat ("large-image25.jpg")
-    convertImageToArchiveFormat ("large-image26.jpg")
-    convertImageToArchiveFormat ("large-image27.jpg")
-    convertImageToArchiveFormat ("large-image28.jpg")
-    convertImageToArchiveFormat ("large-image29.jpg")
-    convertImageToArchiveFormat ("large-image30.jpg")
-    convertImageToArchiveFormat ("large-image31.jpg")
-    convertImageToArchiveFormat ("large-image32.jpg")
-    convertImageToArchiveFormat ("large-image33.jpg")
-    convertImageToArchiveFormat ("large-image34.jpg")
-    convertImageToArchiveFormat ("large-image35.jpg")
-    convertImageToArchiveFormat ("large-image36.jpg")
-    convertImageToArchiveFormat ("large-image37.jpg")
-    convertImageToArchiveFormat ("large-image38.jpg")
-    convertImageToArchiveFormat ("large-image39.jpg")
-    convertImageToArchiveFormat ("large-image40.jpg")
-    convertImageToArchiveFormat ("large-image41.jpg")
-    convertImageToArchiveFormat ("large-image42.jpg")
-    convertImageToArchiveFormat ("large-image43.jpg")
-    convertImageToArchiveFormat ("large-image44.jpg")
-    convertImageToArchiveFormat ("large-image45.jpg")
-    convertImageToArchiveFormat ("large-image46.jpg")
-    convertImageToArchiveFormat ("large-image47.jpg")
-    convertImageToArchiveFormat ("large-image48.jpg")
-    convertImageToArchiveFormat ("large-image49.jpg")
-    convertImageToArchiveFormat ("large-image50.jpg")
-    convertImageToArchiveFormat ("large-image51.jpg")
+    convertImageToArchiveFormat "large-image1.jpg" |> ignore
+    convertImageToArchiveFormat "large-image2.jpg" |> ignore
+    convertImageToArchiveFormat "large-image3.jpg" |> ignore
+    convertImageToArchiveFormat "large-image4.jpg" |> ignore
+    convertImageToArchiveFormat "large-image5.jpg" |> ignore
+    convertImageToArchiveFormat "large-image6.jpg" |> ignore
+    convertImageToArchiveFormat "large-image7.jpg" |> ignore
+    convertImageToArchiveFormat "large-image8.jpg" |> ignore
+    convertImageToArchiveFormat "large-image9.jpg" |> ignore
+    convertImageToArchiveFormat "large-image10.jpg" |> ignore
+    convertImageToArchiveFormat "large-image11.jpg" |> ignore
+    convertImageToArchiveFormat "large-image12.jpg" |> ignore
+    convertImageToArchiveFormat "large-image13.jpg" |> ignore
+    convertImageToArchiveFormat "large-image14.jpg" |> ignore
+    convertImageToArchiveFormat "large-image15.jpg" |> ignore
+    convertImageToArchiveFormat "large-image16.jpg" |> ignore
+    convertImageToArchiveFormat "large-image17.jpg" |> ignore
+    convertImageToArchiveFormat "large-image18.jpg" |> ignore
+    convertImageToArchiveFormat "large-image19.jpg" |> ignore
+    convertImageToArchiveFormat "large-image20.jpg" |> ignore
+    convertImageToArchiveFormat "large-image21.jpg" |> ignore
+    convertImageToArchiveFormat "large-image22.jpg" |> ignore
+    convertImageToArchiveFormat "large-image23.jpg" |> ignore
+    convertImageToArchiveFormat "large-image24.jpg" |> ignore
+    convertImageToArchiveFormat "large-image25.jpg" |> ignore
+    convertImageToArchiveFormat "large-image26.jpg" |> ignore
+    convertImageToArchiveFormat "large-image27.jpg" |> ignore
+    convertImageToArchiveFormat "large-image28.jpg" |> ignore
+    convertImageToArchiveFormat "large-image29.jpg" |> ignore
+    convertImageToArchiveFormat "large-image30.jpg" |> ignore
+    convertImageToArchiveFormat "large-image31.jpg" |> ignore
+    convertImageToArchiveFormat "large-image32.jpg" |> ignore
+    convertImageToArchiveFormat "large-image33.jpg" |> ignore
+    convertImageToArchiveFormat "large-image34.jpg" |> ignore
+    convertImageToArchiveFormat "large-image35.jpg" |> ignore
+    convertImageToArchiveFormat "large-image36.jpg" |> ignore
+    convertImageToArchiveFormat "large-image37.jpg" |> ignore
+    convertImageToArchiveFormat "large-image38.jpg" |> ignore
+    convertImageToArchiveFormat "large-image39.jpg" |> ignore
+    convertImageToArchiveFormat "large-image40.jpg" |> ignore
+    convertImageToArchiveFormat "large-image41.jpg" |> ignore
+    convertImageToArchiveFormat "large-image42.jpg" |> ignore
+    convertImageToArchiveFormat "large-image43.jpg" |> ignore
+    convertImageToArchiveFormat "large-image44.jpg" |> ignore
+    convertImageToArchiveFormat "large-image45.jpg" |> ignore
+    convertImageToArchiveFormat "large-image46.jpg" |> ignore
+    convertImageToArchiveFormat "large-image47.jpg" |> ignore
+    convertImageToArchiveFormat "large-image48.jpg" |> ignore
+    convertImageToArchiveFormat "large-image49.jpg" |> ignore
+    convertImageToArchiveFormat "large-image50.jpg" |> ignore
+    convertImageToArchiveFormat "large-image51.jpg" |> ignore
     convertImageToArchiveFormat "dog.jpg" |> ignore
     convertImageToArchiveFormat "person.jpg" |> ignore
 
